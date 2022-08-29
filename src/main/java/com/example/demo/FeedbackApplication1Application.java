4dc4250aa747bcc88ae74c3d596bd3bfe157ec81
Sujit Sable <sujay@Sujits-MacBook-Pro.local>
2022-08-29 12:05:32 +0530

    Application Started

---
+++ src/main/java/com/example/demo/FeedbackApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackApplication1Application.class, args);
+	}
+
+}
